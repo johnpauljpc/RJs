b911582c6c317e66f2e676f25a44e4a3b8ae8741
johnpauljpc <jpcwork081@gmail.com>
2023-05-28 08:20:05 +0100

    .

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='content App-logo'>
+        <h1>React component </h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
