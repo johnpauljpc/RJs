998f83fcb0656d45fe4ecc0ef6a5ba262e7f95ce
johnpauljpc <jpcwork081@gmail.com>
2023-06-03 22:44:57 +0100

    .

--- src/App.js
+++ src/App.js
@@ -2,10 +2,16 @@
 import './App.css';
 
 function App() {
+  const name = 'Johnpaul';
+  // const List = [{name:'John', age:25}, {name:'paul', age:26}]
+
+ 
   return (
     <div className="App">
-      <div className='content App-logo'>
-        <h1>React component </h1>
+      <div className='content '>
+        <h1 className='App-logo'>Intro </h1>
+        <p>My name is {name}</p>
+        
       </div>
     </div>
   );
