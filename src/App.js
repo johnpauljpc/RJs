7dca80f7eba9d94bb3ac320752d1b665a3f5122c
johnpauljpc <jpcwork081@gmail.com>
2023-06-05 19:43:57 +0100

    .

--- src/App.js
+++ src/App.js
@@ -6,9 +6,11 @@ import Navbar  from './Navbar';
 import Home from './Home';
 
 
-function App() {
+function App(props) {
+  console.log(props)
   // const name = 'Johnpaul';
   // const List = [{name:'John', age:25}, {name:'paul', age:26}]
+  // console.log(List)
 
 
   return (
