5792bc0b19bbf957b8bdcfe8b6a8d82deac9e00b
johnpauljpc <jpcwork081@gmail.com>
2023-06-09 05:27:13 +0100

    used useState hook

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,26 @@
+import { useState } from "react";
 const Home = () => {
+    const [myName, setName] = useState('Johnpaul')
+    
+    const handleClick = () =>{
+        console.log("clicked me!")
+        if (myName ==='Johnpaul'){
+            setName ('JPC')
+        }
+        else{
+            setName ('Johnpaul')
+        }
+    }
+    const clickMeAgain = (name1)=>{
+        console.log(`hello ${name1}`)
+    }
     return ( 
         <div className="home">
-            <p>Home page</p>
+            <p>Home page {myName}</p>
+            <button onClick={handleClick}>click me</button>
+            <button onClick={()=>{
+                clickMeAgain('Johnpaul')
+            }}>Click me again</button>
         </div>
      );
 }
