dcaf8c20847a1105361c6aae9fb68135d6388e47
johnpauljpc <jpcwork081@gmail.com>
2023-06-11 07:34:22 +0100

    .

--- src/Home.js
+++ src/Home.js
@@ -2,9 +2,9 @@ import { useState } from "react";
 const Home = () => {
     const [myName, setName] = useState('Johnpaul')
     const [blogs, setBlogs] = useState([
-        {title:"post1", body:"this is post1", id:1},
-        {title:"post2", body:"this is post2", id:2},
-        {title:"post3", body:"this is post3", id:3}
+        {title:"post1", body:"this is post1", author:"John", id:1},
+        {title:"post2", body:"this is post2", author:"Paul", id:2},
+        {title:"post3", body:"this is post3", author:"Chigozie", id:3}
     ])
     const handleClick = () =>{
         console.log("clicked me!")
@@ -28,13 +28,16 @@ const Home = () => {
 
             
                 {
-                    blogs.map((blog)=>(
+                    blogs.map(blog => (
                         <div className="blog-preview" key={blog.id}>
                         <h2>{blog.title}</h2>
                         <p>{blog.body}</p>
+                        <p>written by: {blog.author}</p>
+
                         </div>
                     ))
                 }
+                
             
         </div>
      );
